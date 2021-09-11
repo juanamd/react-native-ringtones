d80d95fa363a34cc8e9c1fa8d52cea47a67086f9
Juan Story <juanstory@gmail.com>
2021-09-10 23:36:25 -0300

    Change constants variable name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { NativeModules } from "react-native";
 
 const { Ringtones } = NativeModules;
 
-export const RingtoneManager = {
+export const RINGTONE_TYPES = {
 	TYPE_ALL: 7,
 	TYPE_RINGTONE: 1,
 	TYPE_NOTIFICATION: 2,
