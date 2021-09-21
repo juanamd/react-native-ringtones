be91aab55023130b82c9c060e5768fbca7e58c7c
Juan Story <juanstory@gmail.com>
2021-09-21 18:03:35 -0300

    Update type definitions

--- flow-typed/index.js
+++ flow-typed/index.js
@@ -23,8 +23,8 @@ declare module "react-native-ringtones" {
 	declare export default class Ringtones {
 		static getRingtones(type: number): Promise<Ringtone[]>;
 		static getMediaStoreRingtones(): Promise<Ringtone[]>;
-		static getActualRingtone(): Promise<Ringtone>;
-		static setRingtone(uri: string | null): Promise<void>;
+		static getActualRingtone(type: number): Promise<Ringtone>;
+		static setRingtone(uri: string | null, type: number): Promise<void>;
 		static setNewRingtone(params: RingtoneParams): Promise<Ringtone>;
 		static deleteRingtone(uri: string): Promise<number>;
 		static hasSettingsPermission(): Promise<boolean>;
